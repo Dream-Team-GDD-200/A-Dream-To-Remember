a665f211753be7dbfc330e96f7069a2f11b6ea09
Zachary-Boehm <60978390+Zachary-Boehm@users.noreply.github.com>
2020-02-16 11:13:11 -0600

    Fixed Collision and the transition of box and hole

--- Assets/Scripts/HoleCollision.cs
+++ Assets/Scripts/HoleCollision.cs
@@ -4,31 +4,39 @@ using UnityEngine;
 public class HoleCollision : MonoBehaviour
 {
     bool BoxFallFlag = false;
-    bool BoxHasFalled = false;
-    float rateOfFall = .1f;
-    public Vector2 FinalPosition;
-    public Vector2 direction;
+    
+    float lerpPosition = 0.0f;
+    float lerpTime = 1.0f; // This is the number of seconds the Lerp will take
+    Vector3 start;
+    Vector3 end;
+    GameObject puzzleBox, Hole;
     // Start is called before the first frame update
     void Start()
     {
+        puzzleBox = GameObject.Find("Puzzle Box");
+        Hole = GameObject.Find("Hole");
+        end = this.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (BoxFallFlag)
+        {
+            lerpPosition += Time.deltaTime / lerpTime;
+            puzzleBox.transform.position = Vector3.Lerp(start, end, lerpPosition);
+        }
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
         //Checks if the Puzzle box has collided
         if (other.gameObject.name.Equals("Puzzle Box"))
         {
-            //set both objects for hole collision to disapear and not be active
-            GameObject.Find("Hole").SetActive(false);
-            this.gameObject.SetActive(false);
-            //move the box to the holes position
-            other.transform.position = transform.position;
-            //make it so the box cant be collided with
+            start = puzzleBox.transform.position;
             other.isTrigger = true;
+            Hole.SetActive(false);
+            Hole.SetActive(false);
+            BoxFallFlag = true;
         }
     }
 }

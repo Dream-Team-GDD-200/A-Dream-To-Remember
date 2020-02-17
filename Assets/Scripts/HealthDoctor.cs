8bdc9a512c1122b8dd133acb24e6991d441c93e1
J <cockatoo242@gmail.com>
2020-02-17 08:48:09 -0600

    Added functions to health-doctor script

--- Assets/Scripts/HealthDoctor.cs
+++ Assets/Scripts/HealthDoctor.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class HealthDoctor : MonoBehaviour
 {
+
+    public float health = 100f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,28 @@ public class HealthDoctor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // If the doctors health goes below or equal to 0
+        if (health <= 0)
+        {
+            died();
+        }
+    }
+
+    // Used to have the player take damage.  Pass in the amount of damage to take
+    public void takeDamage(float dmg)
+    {
+        health -= dmg;
+    }
+
+    // Used to have the player heal.  Pass in the amount to heal
+    public void heal(float healAmt)
+    {
+        health += healAmt;
+    }
+
+    // called when the player died
+    private void died()
+    {
+
     }
 }

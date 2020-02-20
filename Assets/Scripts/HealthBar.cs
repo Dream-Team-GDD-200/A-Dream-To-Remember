51ee831c185739bf04a65eaa0c91c139bf4d8cc6
nydahle8784 <50083364+nydahle8784@users.noreply.github.com>
2020-02-20 10:13:56 -0600

    HealthBar
    added a health bar-- it ends the game if it reaches zero and i prevented the player from doing anything during death state-- press f to test damage. I couldn't put anything into doctor prefab folder for somereason so if we delete him we'll have to reassign some of the new vars i gave him

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+
+    public Slider slider; //creates a reference to our slider--which is the fill portion of the health bar
+    public void SetMaxHealth(float health) //this may be used later on enemies or something... 
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+    public void SetHealth(float health) //takes in a float and sets health equal to that number
+    {
+        slider.value = health;
+    }
+}
--- Assets/Scripts/HealthDoctor.cs
+++ Assets/Scripts/HealthDoctor.cs
@@ -1,43 +1,74 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor; //used to end game if dead
 using UnityEngine;
+
 
 public class HealthDoctor : MonoBehaviour
 {
 
-    public float health = 100f;
-
+    public float currentHealth = 100f;
+    public float maxHealth = 100f;
+    public HealthBar healthBar;
+    public GameObject joyStick; //reference to joystick to stop movement when dead 
+    public GameObject buttons; //reference to buttons to stop being able to press buttons when dead
+    public GameObject player; //reference to player to stop
     // Start is called before the first frame update
+    
     void Start()
     {
-        
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
         // If the doctors health goes below or equal to 0
-        if (health <= 0)
+        if (currentHealth <= 0)
         {
             died();
+        }
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            takeDamage(10f);
         }
     }
 
     // Used to have the player take damage.  Pass in the amount of damage to take
     public void takeDamage(float dmg)
     {
-        health -= dmg;
+        currentHealth -= dmg;
+        healthBar.SetHealth(currentHealth);
     }
 
     // Used to have the player heal.  Pass in the amount to heal
     public void heal(float healAmt)
     {
-        health += healAmt;
+        currentHealth += healAmt;
     }
 
     // called when the player died
-    private void died()
+    private void died() //might be better to switch over to a completly different scene if this function causes problems later down the line
     {
+        //make joystick disappear
+        joyStick.SetActive(false); ;
+        //make buttons disappear ***white blood cells will still keep moving--collision boxes might need to be turned off
+        buttons.SetActive(false); //wasn't working 
+        //stops movement of doctor and possibly other things
+        player.GetComponent<PlayerMovement>().enabled = false; //*** might not be the greatest way to stop him, replace later if better method is found
+        //*** ask joel to stop his animations here
+        
+        //want to put somekind of gameOver death here
+        //game over text or something here
+        Invoke("endGame",2); //ends game in 2 seconds
 
     }
+    private void endGame()
+    {
+        if (EditorApplication.isPlaying)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+    }
+     
 }

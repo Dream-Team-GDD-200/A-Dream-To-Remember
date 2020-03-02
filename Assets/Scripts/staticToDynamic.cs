292584b3469d3cb774f1e69b0e99464a059b3999
nydahle8784 <50083364+nydahle8784@users.noreply.github.com>
2020-03-02 11:03:53 -0600

    boxes no bullying

---
+++ Assets/Scripts/staticToDynamic.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class staticToDynamic : MonoBehaviour
+{
+     private Rigidbody2D body;
+    private bool isPushing = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter2D(Collision2D other) //mass of boxes start out very high and are only moveable when it collides with player
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+      
+            body.mass = 1;
+
+        }
+
+    }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        body.mass = 10000;
+    }
+}

97b67ae83bcb9e14ea60d5a8d7e8b87b06a7c22d
levendowskil3713 <60446780+levendowskil3713@users.noreply.github.com>
2020-03-02 10:52:12 -0600

    Finished third skill
    Finished third skill

--- Assets/Scripts/EnablePathfinding.cs
+++ Assets/Scripts/EnablePathfinding.cs
@@ -5,6 +5,7 @@ using Pathfinding;
 
 public class EnablePathfinding : MonoBehaviour
 {
+    public bool pathfindingActive = true;
     public AIPath aipath;
     public GameObject Player;
     public GameObject Room;
@@ -12,9 +13,21 @@ public class EnablePathfinding : MonoBehaviour
     void Update()
     {
         //Checks to see if the player is in the room with the enemy (will make this more industrial later)
-        if(Player.transform.position.x > (Room.transform.position.x - (Room.transform.lossyScale.x / 4)) && Player.transform.position.x < (Room.transform.position.x + (Room.transform.lossyScale.x / 4)) && Player.transform.position.y > (Room.transform.position.y - (Room.transform.lossyScale.y / 4)) && Player.transform.position.y < (Room.transform.position.y + (Room.transform.lossyScale.y / 4)))
+        if(pathfindingActive == true && Player.transform.position.x > (Room.transform.position.x - (Room.transform.lossyScale.x / 4)) && Player.transform.position.x < (Room.transform.position.x + (Room.transform.lossyScale.x / 4)) && Player.transform.position.y > (Room.transform.position.y - (Room.transform.lossyScale.y / 4)) && Player.transform.position.y < (Room.transform.position.y + (Room.transform.lossyScale.y / 4)))
         {
             this.gameObject.GetComponent<AIPath>().enabled = true;
         }
     }
+
+  public void disablePathfinding()
+  {
+    this.gameObject.GetComponent<AIPath>().enabled = false;
+    pathfindingActive = false;
+  }
+
+  public void enablePathfinding()
+  {
+    this.gameObject.GetComponent<AIPath>().enabled = true;
+    pathfindingActive = true;
+  }
 }

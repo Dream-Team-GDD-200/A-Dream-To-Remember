94168c0da25712af596c7b0bc600b8be69344e97
levendowskil3713 <60446780+levendowskil3713@users.noreply.github.com>
2020-03-02 09:46:13 -0600

    Started third skill
    Started third skill

---
+++ Assets/Scripts/HealEffect.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealEffect : MonoBehaviour
+{
+  public Transform fireLocation;
+  public GameObject baseEffectSmall;
+  public GameObject effectSmall;
+  public GameObject baseEffectLarge;
+  public GameObject effectLarge;
+  private PlayerMovement playerMovement;
+
+  void Start()
+  {
+    //Creates a projectile and deployable cell off screen. This allows the Shoot and Deploy functions to call parent objects that cannot be destroyed.
+    baseEffectSmall = Instantiate(baseEffectSmall, new Vector3(2000, 2000, 2000), fireLocation.rotation);
+    baseEffectLarge = Instantiate(baseEffectLarge, new Vector3(2000, 2000, 2000), fireLocation.rotation);
+
+    playerMovement = GetComponent<PlayerMovement>();
+  }
+
+  // Start is called before the first frame update
+  void Update()
+  {
+  }
+
+  // Update is called once per frame
+  public void Heal()
+  {
+    effectSmall = Instantiate(baseEffectSmall, fireLocation.position, fireLocation.rotation);
+    Destroy(effectSmall, 0.2f);
+    effectLarge = Instantiate(baseEffectLarge, fireLocation.position, fireLocation.rotation);
+    Destroy(effectLarge, 0.1f);
+  }
+}
---
+++ Assets/Scripts/StunField.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StunField : MonoBehaviour
+{
+  public Transform fireLocation;
+  public GameObject baseShockField;
+  public GameObject shockField;
+  private PlayerMovement playerMovement;
+
+  void Start()
+  {
+    //Creates a projectile and deployable cell off screen. This allows the Shoot and Deploy functions to call parent objects that cannot be destroyed.
+    baseShockField = Instantiate(baseShockField, new Vector3(2000, 2000, 2000), fireLocation.rotation);
+
+    playerMovement = GetComponent<PlayerMovement>();
+  }
+
+  // Start is called before the first frame update
+  void Update()
+  {
+  }
+
+  // Update is called once per frame
+  public void Shock()
+  {
+    shockField = Instantiate(baseShockField, fireLocation.position, fireLocation.rotation);
+    Destroy(shockField, 0.2f);
+  }
+}

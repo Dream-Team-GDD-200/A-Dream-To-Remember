066d327ba1a48a86f6c2bc9fb09c07df9383bfd8
J <cockatoo242@gmail.com>
2020-03-04 09:03:30 -0600

    BG Audio and Projectile Sound Collision

--- Assets/Scripts/Projectile_Destruction.cs
+++ Assets/Scripts/Projectile_Destruction.cs
@@ -4,13 +4,39 @@ using UnityEngine;
 
 public class Projectile_Destruction : MonoBehaviour
 {
+
+    private AudioSource audioS;
+    private SpriteRenderer spr;
+
+    void Start()
+    {
+        audioS = this.gameObject.GetComponent<AudioSource>();
+        spr = this.gameObject.GetComponent<SpriteRenderer>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
+
         // if colliding with wall, box, deployed cell, or enemy projectile is destroyed
         if (other.gameObject.CompareTag("Wall") || other.gameObject.CompareTag("Box") || other.gameObject.CompareTag("DeployedCell") || other.gameObject.CompareTag("Enemy"))
         {
-            // destroy object
-            Destroy(this.gameObject);
+            audioS.Play();
+
+            spr.enabled = false;
+            
+            StartCoroutine(waitForSound());
         }
+    }
+
+    IEnumerator waitForSound()
+    {
+        //Wait Until Sound has finished playing
+        while (audioS.isPlaying)
+        {
+            yield return null;
+        }
+
+        //Auidio has finished playing, destroy GameObject
+        Destroy(this.gameObject);
     }
 }

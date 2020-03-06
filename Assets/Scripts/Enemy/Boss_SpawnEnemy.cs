a12fb689238e1933b301d43d68aa211affd1b076
Zachary-Boehm <60978390+Zachary-Boehm@users.noreply.github.com>
2020-03-06 14:15:18 -0600

    Boss AI progress

--- Assets/Scripts/Enemy/Boss_SpawnEnemy.cs
+++ Assets/Scripts/Enemy/Boss_SpawnEnemy.cs
@@ -8,34 +8,48 @@ public class Boss_SpawnEnemy : MonoBehaviour
     // SpawnHord will enable or disable the spawn of the enemies
     public GameObject enemy;
     public bool SpawnHord;
-    public float spawnTime;
-    public float spawnDelay;
+    public float spawnTime, spawnDelay;
+    public float amountOfEnemies;
     public AIPath bossPath;
     void Start()
     {
-        if (SpawnHord)
-        {
-            CallEnemySpawn();
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        
     }
     public void CallEnemySpawn()
     {
         Invoke("SpawnEnemy", spawnTime);
-        Invoke("SpawnEnemy", spawnTime + spawnDelay);
-        //Invoke("SpawnEnemy", spawnTime + spawnDelay + spawnDelay);
-        
+        for(int i = 0; i < amountOfEnemies; i++)
+        {
+            Invoke("SpawnEnemy", spawnDelay);
+        }
     }
+    //will spawn enemies only if you have the feature turned on
     public void SpawnEnemy()
     {
         if (SpawnHord)
         {
             Instantiate(enemy, transform.position, transform.rotation);
         }
+    }
+    public void StartSpawner()
+    {
+        StartCoroutine(StartCountdown());
+    }
+    float currCountdownValue;
+    public IEnumerator StartCountdown(float countdownValue = 10)
+    {
+        currCountdownValue = countdownValue;
+        while (currCountdownValue > 0)
+        {
+            Debug.Log("Countdown: " + currCountdownValue);
+            yield return new WaitForSeconds(1.0f);
+            
+            currCountdownValue--;
+            if (currCountdownValue == 0)
+            {
+                CallEnemySpawn();
+            }
+        }
+        StartCoroutine(StartCountdown());
     }
 }

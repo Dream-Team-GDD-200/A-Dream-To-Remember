0d429c768024d0a1221528389237638dba2121db
nydahle8784 <50083364+nydahle8784@users.noreply.github.com>
2020-03-08 18:08:22 -0500

    sdfgd
    gfds

---
+++ Assets/Scripts/Enemy/SetDestination.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class SetDestination : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.GetComponent<AIDestinationSetter>().target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/WhiteBloodCells/BROKENBarrier_Destruction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BROKENBarrier_Destruction : MonoBehaviour
+{
+  // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy") || other.gameObject.CompareTag("Projectile"))
+        {
+          GameObject.FindGameObjectWithTag("Player").GetComponent<WhiteBloodCell>().reduceCellHealth(1);
+        }
+
+        int health = GameObject.FindGameObjectWithTag("Player").GetComponent<WhiteBloodCell>().getCellHealth();
+        if (health <= 0)
+            {
+              Destroy(this.gameObject);
+            }
+    }
+}

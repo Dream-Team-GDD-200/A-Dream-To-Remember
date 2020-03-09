0a195b1a1d1b4a0f956168cd8b3283d102b156d1
nydahle8784 <50083364+nydahle8784@users.noreply.github.com>
2020-03-09 10:57:19 -0500

    Added Rune script, box works without triggers now

--- Assets/Scripts/boxPushable.cs
+++ Assets/Scripts/boxPushable.cs
@@ -2,31 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class boxPushable : MonoBehaviour
+public class BoxPushable : MonoBehaviour
 {
-    Rigidbody2D phys;
+    private Rigidbody2D phys;
     void Start()
     {
         phys = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
 
-        if (!collision.isTrigger && collision.tag == "Player") ///if a collider on the player and the collider is not the trigger colider (the capsul colider)
+        if (collision.gameObject.tag == "Player") ///if a collider on the player and the collider is not the trigger colider (the capsul colider)
         {
             phys.bodyType = RigidbodyType2D.Dynamic;
         }
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        phys.bodyType = RigidbodyType2D.Static;
+        else
+        {
+            phys.bodyType = RigidbodyType2D.Static;
+        }
     }
 
-    private void OnTriggerStay2D(Collider2D collision) //as long as it stay attacfhed to box it stays dynamic
+    private void OnCollisionStay2D(Collision2D collision) //as long as it stay attacfhed to box it stays dynamic
     {
-        if (!collision.isTrigger && collision.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
             phys.bodyType = RigidbodyType2D.Dynamic;
         }

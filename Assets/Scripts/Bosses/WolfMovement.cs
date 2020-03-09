d97f23f7487b9d7fc07204d25111ff4e09a647d9
J <cockatoo242@gmail.com>
2020-03-09 10:13:17 -0500

    Started Adding Wolf Stuff

---
+++ Assets/Scripts/Bosses/WolfMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+
+    // Handle animation
+    // 1 - idle down | 2 - idle up | 3 - idle right | 4 - idle left
+    // -1 - walk down | -2 - walk up | -3 - walk right | -4 - walk left
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}

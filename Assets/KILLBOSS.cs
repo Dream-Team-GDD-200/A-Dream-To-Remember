a02e882c1bf119f1987873cc976d5bd4279a7f09
nydahle8784 <50083364+nydahle8784@users.noreply.github.com>
2020-03-09 23:38:15 -0500

    Fixed
    Fixed

--- Assets/KILLBOSS.cs
+++ Assets/KILLBOSS.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class KILLBOSS : MonoBehaviour
 {
+    public Sprite left;
+    bool hasBeenPushed = false;
+    public GameObject Boss;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,14 @@ public class KILLBOSS : MonoBehaviour
     void Update()
     {
         
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player" && hasBeenPushed == false && other is BoxCollider2D)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = left;
+            hasBeenPushed = true;
+            Boss.SetActive(false);
+        }
     }
 }

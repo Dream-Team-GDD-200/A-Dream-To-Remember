aa02ee18c6f7084f78e6163e6a1585ee59b569f6
Zachary-Boehm <60978390+Zachary-Boehm@users.noreply.github.com>
2020-03-30 12:05:51 -0500

    Basic Pause Menu

--- Assets/Scripts/UI/PauseController.cs
+++ Assets/Scripts/UI/PauseController.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Pathfinding;
+using UnityEngine.SceneManagement;
+
 public class PauseController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject pauseOverlay;
+    public void openMenu()
     {
-        
+        pauseOverlay.SetActive(true);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void closeMenu()
     {
-        
+        pauseOverlay.SetActive(false);
+    }
+    public void worldSelect()
+    {
+        SceneManager.LoadScene("Overworld");
+    }
+    public void characterSelect()
+    {
+        SceneManager.LoadScene("Title Screen");
     }
 }

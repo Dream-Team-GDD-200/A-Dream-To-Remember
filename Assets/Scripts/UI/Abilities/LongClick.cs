75320c9143deb4d05890ce3f347598320c2fab94
Zachary-Boehm <60978390+Zachary-Boehm@users.noreply.github.com>
2020-03-31 16:34:50 -0500

    Ability Delays

--- Assets/Scripts/UI/Abilities/LongClick.cs
+++ Assets/Scripts/UI/Abilities/LongClick.cs
@@ -5,109 +5,29 @@ using UnityEngine;
 
 public class LongClick : MonoBehaviour
 {
-    //public Vector3 buttonPos;
-    //public Image skillImage;
-    ////used to determine how long the player holds down the mouse button
-    //private float startTime, endTime;
-    ////current cooldown time
-    //private float cooldownTime;
-    ////the total cooldown time
-    //private float cooldownMaxTime = 125;
-    ////the radous of the button
-    //private double buttonRad = 60;
+    public float DeployCellCooldown, HealCooldown, ShockCooldown, NurseCooldown; // The time for each skill cooldown
+    public float delay; // duration is how long it waits until it fills the next section of the cooldown... Delay is how many times a second the fill is updated
+    float duration;
+    private float CellDuration = 0f, HealDuration = 0f, ShockDuration = 0f, NurseDuration = 0f; // the current time left of each cooldown
+    public Image DeployImage, HealImage, ShockImage, NurseImage; // the image reference for the fill animation
+    
+    public AudioClip speedBoost; // audiio clip for the speed boost
 
-    public AudioClip speedBoost;
-
-
-  //  //Use for initialization
-  //  private void Start()
-  //  {
-  //      startTime = 0f;
-  //      endTime = 0f;
-  //  }
-
-  ////checks to see if the player's mouse is on top of the button (this likely could be improved)
-  //private bool CheckBounds()
-  //  {
-  //      buttonPos = GameObject.Find("Skill1 BG").transform.position;
-
-
-  //      if ((Input.mousePosition.x >= (buttonPos.x- buttonRad)) && (Input.mousePosition.x <= (buttonPos.x + buttonRad)) && (Input.mousePosition.y >= (buttonPos.y - buttonRad)) && (Input.mousePosition.y <= (buttonPos.y + buttonRad)) && cooldownTime == 0)
-  //      {
-  //          return true;
-  //      }
-  //      else
-  //      {
-  //          return false;
-  //      }
-
-  //      /*
-  //      if ((Input.mousePosition.x >= 890) && (Input.mousePosition.x <= 995) && (Input.mousePosition.y >= 20) && (Input.mousePosition.y <= 120))
-  //      {
-  //          return true;
-  //      }
-  //      else
-  //      {
-  //          return false;
-  //      }
-  //      */
-  //  }
-
-  //  void Update()
-  //  {
-  //      //sets the fill amount for the skill button
-  //      skillImage.fillAmount = (float)1 - (cooldownTime / cooldownMaxTime);
-
-  //      //keeps track of how long the mouse is being held down for if the mouse is on the button
-  //      if (Input.GetMouseButtonDown(0) && CheckBounds() == true)
-  //      {
-  //          startTime = Time.time;
-  //          GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>().SetBool("attacking", true);
-  //      }
-  //      if (Input.GetMouseButtonUp(0) && CheckBounds() == true)
-  //      {
-  //          endTime = Time.time;
-  //      }
-
-  //      //fires a cell if the mouse is held down for less that 1/2 of a second
-  //      if (endTime - startTime > 0.5f && Input.mousePosition.x >= 786 && CheckBounds() == true && cooldownTime == 0)
-  //      {
-  //          Debug.Log("Long Click");
-  //          startTime = 0f;
-  //          endTime = 0f;
-  //          GameObject.FindGameObjectWithTag("Player").GetComponent<WhiteBloodCell>().Deploy();
-  //          cooldownTime = cooldownMaxTime;
-  //          StartCoroutine(waitForAnimDeploy());
-  //      }
-  //      //deploys a cell if the mouse is held down for at least 1/2 of a second
-  //      if ((endTime - startTime <= 0.5f) && (endTime - startTime > 0.001) && CheckBounds() == true && cooldownTime == 0)
-  //      {
-  //          Debug.Log("Short Click");
-  //          startTime = 0f;
-  //          endTime = 0f;
-  //          GameObject.FindGameObjectWithTag("Player").GetComponent<WhiteBloodCell>().Shoot(new Vector2(0,0));
-  //          cooldownTime = cooldownMaxTime;
-  //          StartCoroutine(waitForAnimDeploy());
-  //      }
-  //      //reduces the cooldown time if it is less than 0 each timestep
-  //      if (cooldownTime > 0)
-  //      {
-  //          cooldownTime -= 1;
-  //      }
-  //  }
-
+    //delay for the attacking animation
     IEnumerator waitForAnimDeploy()
     {
         yield return new WaitForSeconds(0.417f);
 
         GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>().SetBool("attacking", false);
     }
+    //delay for the healing animation until it stops
     IEnumerator waitForAnimHeal()
     {
         yield return new WaitForSeconds(0.75f);
         GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().resetSpeed();
         GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>().SetBool("healing", false);
     }
+    // delay for the shock animation to stop
     IEnumerator waitForAnimShock()
     {
         yield return new WaitForSeconds(0.833f);
@@ -116,32 +36,106 @@ public class LongClick : MonoBehaviour
     //this is function is to deploy the cell when the button is clicked
     public void deployCell()
     {
-        GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>().SetBool("attacking", true);
-        GameObject.FindGameObjectWithTag("Player").GetComponent<WhiteBloodCell>().Deploy();
-        StartCoroutine(waitForAnimDeploy());
+        if (CellDuration == 0)
+        {
+            GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>().SetBool("attacking", true);
+            GameObject.FindGameObjectWithTag("Player").GetComponent<WhiteBloodCell>().Deploy();
+            StartCoroutine(waitForAnimDeploy());
+            cooldown(1);
+        }
     }
+    // does the heal player animation, sounds, and logic
     public void healPlayer()
     {
         Debug.Log("Small Heal");
-
-        GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>().SetBool("healing", true);
-        float healVal = 15;
-        GameObject.FindGameObjectWithTag("Player").GetComponent<HealthDoctor>().heal(healVal);
-        float speed = 5f;
-        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().setSpeed(speed);
-        GameObject.FindGameObjectWithTag("Player").GetComponent<HealEffect>().Heal();
-        GameObject.FindGameObjectWithTag("Player").GetComponent<AudioSource>().clip = speedBoost;
-        GameObject.FindGameObjectWithTag("Player").GetComponent<AudioSource>().Play();
-        StartCoroutine(waitForAnimHeal());
+        if (HealDuration == 0)
+        {
+            GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>().SetBool("healing", true);
+            float healVal = 15;
+            GameObject.FindGameObjectWithTag("Player").GetComponent<HealthDoctor>().heal(healVal);
+            float speed = 5f;
+            GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().setSpeed(speed);
+            GameObject.FindGameObjectWithTag("Player").GetComponent<HealEffect>().Heal();
+            GameObject.FindGameObjectWithTag("Player").GetComponent<AudioSource>().clip = speedBoost;
+            GameObject.FindGameObjectWithTag("Player").GetComponent<AudioSource>().Play();
+            StartCoroutine(waitForAnimHeal());
+            cooldown(2);
+        }
     }
+    // does the shock enemy logic
     public void shockEnemy()
     {
-        GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>().SetBool("shocking", true);
-        GameObject.FindGameObjectWithTag("Player").GetComponent<StunField>().Shock();
-        StartCoroutine(waitForAnimShock());
+        if (ShockDuration == 0)
+        {
+            GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>().SetBool("shocking", true);
+            GameObject.FindGameObjectWithTag("Player").GetComponent<StunField>().Shock();
+            StartCoroutine(waitForAnimShock());
+            cooldown(3);
+        }
     }
+    // does the nurse logic
     public void spawnNurse()
     {
-        GameObject.FindGameObjectWithTag("Player").GetComponent<NurseSpawn>().Spawn();
+        if (NurseDuration == 0)
+        {
+            GameObject.FindGameObjectWithTag("Player").GetComponent<NurseSpawn>().Spawn();
+            cooldown(4);
+        }
+    }
+    //starts the cooldown for a specific ability
+    void cooldown(int skill)
+    {
+        duration = 1 / delay;
+        switch (skill)
+        {
+            case 1:
+                CellDuration = DeployCellCooldown;
+                DeployImage.fillAmount = 0;
+                StartCoroutine(CoolDown(CellDuration, 1));
+                break;
+            case 2:
+                HealDuration = HealCooldown;
+                HealImage.fillAmount = 0;
+                StartCoroutine(CoolDown(HealDuration, 2));
+                break;
+            case 3:
+                ShockDuration = ShockCooldown;
+                StartCoroutine(CoolDown(ShockDuration, 3));
+                break;
+            case 4:
+                NurseDuration = NurseCooldown;
+                StartCoroutine(CoolDown(NurseDuration, 4));
+                break;
+        }
+    }
+    //Cooldown for a given ability
+    IEnumerator CoolDown(float time, int type)
+    {
+        time *= delay;
+        while (time > 0)
+        {
+            yield return new WaitForSeconds(duration);
+
+            time--;
+            switch (type)
+            {
+                case 1:
+                    CellDuration -= duration;
+                    DeployImage.fillAmount = (float)1 - (CellDuration / DeployCellCooldown);
+                    break;
+                case 2:
+                    HealDuration-= duration;
+                    HealImage.fillAmount = (float)1 - (HealDuration / HealCooldown);
+                    break;
+                case 3:
+                    ShockDuration-= duration;
+                    ShockImage.fillAmount = (float)1 - (ShockDuration / ShockCooldown);
+                    break;
+                case 4:
+                    NurseDuration-= duration;
+                    NurseImage.fillAmount = (float)1 - (NurseDuration / NurseCooldown);
+                    break;
+            }
+        }
     }
 }

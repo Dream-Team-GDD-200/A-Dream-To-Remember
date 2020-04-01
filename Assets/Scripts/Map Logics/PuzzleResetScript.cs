d8954e66ad0a2e5bab393ad715b012ec4d8453da
nydahle8784 <50083364+nydahle8784@users.noreply.github.com>
2020-04-01 11:15:13 -0500

    Added sceneTranstions
    Added a generic sceneTransion prefab in UI --added a special case transition for puzzleResetSwitch

--- Assets/Scripts/Map Logics/PuzzleResetScript.cs
+++ Assets/Scripts/Map Logics/PuzzleResetScript.cs
@@ -12,12 +12,20 @@ public class PuzzleResetScript : MonoBehaviour
     private SpriteRenderer resetButton; //temp using this to tell when button has been pressed
     bool hasBeenPushed = false;
     private GameObject tempSwitchHolder;
- 
+
+    private IEnumerator fade;
+    private IEnumerator unFade;
+
+    private bool doingCoroutine = false;
+
+    private GameObject puzzleResetTransition;
     // Start is called before the first frame update
     void Start()
     {
         resetButton = GetComponent<SpriteRenderer>();
         resetPuzzleMenu.SetActive(false); //set the menu as not active by default
+        puzzleResetTransition = GameObject.FindGameObjectWithTag("Transition");
+
     }
 
     // Update is called once per frame
@@ -33,31 +41,57 @@ public class PuzzleResetScript : MonoBehaviour
             resetButton.color = Color.green; //indicate button is currently pressed down
             resetPuzzleMenu.SetActive(true); //menu is active now
 
-            resetPuzzleMenu.GetComponent<PuzzleResetMenuHandler>().currentResetSwitch = this.gameObject;
-            Time.timeScale = 0f; //pauses game during this time
-            //this.GetComponent<GameObject>(); //sets the reference in the resetPuzzleMenuHandler to this gameswitch
+            resetPuzzleMenu.GetComponent<PuzzleResetMenuHandler>().currentResetSwitch = this.gameObject; //sets the current reset switch to the one that was just pressed
+            Time.timeScale = 0f; //pauses game during this time -- this line is kinda redundant with FadeOut, but I want to make sure that time is stopped when the player is selecting an option for the reset puzzle menu
         }
     }
     public void ResetPuzzle() //player pressed yes on resetPuzzleMenu, will reset puzzle to prefab default settings
     {
-     
-        Destroy(currentPrefabInstance); //destroys current prefab instance
-        currentPrefabInstance = (GameObject)Instantiate(refToPrefabDefault);
-        resetPuzzleMenu.SetActive(false);
-        Time.timeScale = 1f; //unpauses game
-        Invoke("AllowPlayerToPressButtonAgain", 2f);
+
+       StartCoroutine(fadeInAndOut());
 
     }
     public void DontResetPuzzle() //player pressed no on resetPuzzleMenu, returns game to normal
     {
         resetPuzzleMenu.SetActive(false);
-        Time.timeScale = 1f; //unpuases game
+        Time.timeScale = 1f; //unpauses game  -- this one is still needed in this function (unlike the restpuzzle function) as neither fade functions are called here
         Invoke("AllowPlayerToPressButtonAgain", 2f);
     }
     public void AllowPlayerToPressButtonAgain()
     {
         hasBeenPushed = false;
         resetButton.color = Color.white;
+    }
+
+
+    private IEnumerator fadeInAndOut() //dont judge me, it works --unique version of it that requires it to be resued as it needs to be executed in a very specific order
+    {
+        Time.timeScale = 0; //pause game during transition (we probably don't want them to be able to move during black screen
+        CanvasGroup blackSquare = puzzleResetTransition.GetComponent<CanvasGroup>();
+        while (blackSquare.alpha < 1)
+        {
+            blackSquare.alpha += Time.unscaledDeltaTime / 2;
+            yield return null; //wait for next frame before doing it again
+        }
+        yield return null; // extra assurance that it is done 
+                           //changing timescale back to 1 seems unnecessary as when the DoFade function is called (which will almost always be called soon after this one) it resets time back to 1, and if we are changing to a new scene it doesn't matter since time will reset
+
+        Destroy(currentPrefabInstance); //destroys current prefab instance
+        currentPrefabInstance = (GameObject)Instantiate(refToPrefabDefault);
+        resetPuzzleMenu.SetActive(false);
+
+
+    
+        while (blackSquare.alpha > 0)
+        {
+            blackSquare.alpha -= Time.unscaledDeltaTime / 2;
+            yield return null; //wait for next frame before doing it again
+        }
+        yield return null; // extra assurance that it is done 
+        Time.timeScale = 1; //now that we know for sure that it is done, unpause the game
+
+
+        Invoke("AllowPlayerToPressButtonAgain", 2f); // after two seconds, button is pressable again
     }
 }
 

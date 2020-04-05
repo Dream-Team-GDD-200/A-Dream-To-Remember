49ae99dd64b8185ca3a2029ef4ab07c2bb1ddbdd
J <cockatoo242@gmail.com>
2020-04-05 17:46:48 -0500

    Fixed Enemies and Movement

--- Assets/Projectile_controller.cs
+++ Assets/Projectile_controller.cs
@@ -7,8 +7,6 @@ public class Projectile_controller : MonoBehaviour
 
     private Vector2 movement = new Vector2(0, -1);
 
-    private bool isMoving = false;
-
     private Animator anim;
     public Vector2 ProjectileMobMovement;
     public AIPath ProjectileMob;
@@ -25,75 +23,35 @@ public class Projectile_controller : MonoBehaviour
     // -1 - walk down | -2 - walk up | -3 - walk right | -4 - walk left
     void Update()
     {
+        
         if (movement.y > Mathf.Abs(movement.x))
         {
-            //Facing Up
-            if (isMoving)
-            {
                 //Walking Up
                 anim.SetInteger("direction", -2);
-            }
-            else
-            {
-                //Idle Up
-                anim.SetInteger("direction", 2);
-            }
+            Debug.Log("here 1");
         }
         else if (Mathf.Abs(movement.y) > Mathf.Abs(movement.x))
         {
-            //Facing Down
-            if (isMoving)
-            {
                 //walking down
                 anim.SetInteger("direction", -1);
-            }
-            else
-            {
-                //idle down
-                anim.SetInteger("direction", 1);
-            }
+            Debug.Log("here 2");
         }
         else if (movement.x > Mathf.Abs(movement.y))
         {
-            //Facing right
-            if (isMoving)
-            {
                 //walking right
                 anim.SetInteger("direction", -3);
-            }
-            else
-            {
-                //idle right
-                anim.SetInteger("direction", 3);
-            }
+            Debug.Log("here 3");
         }
         else if (Mathf.Abs(movement.x) > Mathf.Abs(movement.y))
         {
-            //Facing left
-            if (isMoving)
-            {
                 //walking left
                 anim.SetInteger("direction", -4);
-            }
-            else
-            {
-                //idle left
-                anim.SetInteger("direction", 4);
-            }
+            Debug.Log("here 4");
         }
     }
 
     void FixedUpdate()
     {
-        //TODO: Set isMoving to true when moving and false when stopped
-        if (ProjectileMobMovement == new Vector2(0, 0))
-        {
-            isMoving = false;
-        }
-        else
-        {
-            isMoving = true;
-        }
         //TODO: Updated the movement Vector2 when the wolf is moving or stopped
         ProjectileMobMovement = ProjectileMob.velocity;
         movement = ProjectileMobMovement;

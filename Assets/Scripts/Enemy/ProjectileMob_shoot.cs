d424680aff10fd86e2a377c1488c62074daaceac
Zachary-Boehm <60978390+Zachary-Boehm@users.noreply.github.com>
2020-04-05 18:19:48 -0500

    Projectile Enemy Logic

---
+++ Assets/Scripts/Enemy/ProjectileMob_shoot.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMob_shoot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject projectile;
+    private GameObject Player;
+    private Vector2 PlayerPosition;
+    private Vector2 Force;
+    public float projectileSpeed;
+    public int timeBetweenShot;
+
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        PlayerPosition = Player.transform.position;
+        StartCoroutine(shoot());
+    }
+
+    private void Update()
+    {
+        PlayerPosition = Player.transform.position;
+
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+
+    IEnumerator shoot()
+    {
+        yield return new WaitForSeconds(timeBetweenShot);
+
+        GameObject projectileTemp;
+        projectileTemp = Instantiate(projectile, this.transform.position, Quaternion.identity);
+        projectileTemp.GetComponent<Rigidbody2D>().velocity = (Player.transform.position - projectileTemp.transform.position).normalized * projectileSpeed;
+        projectileTemp.transform.parent = GameObject.FindGameObjectWithTag("Projectile_Parent").transform;
+
+        StartCoroutine(shoot());
+    }
+}

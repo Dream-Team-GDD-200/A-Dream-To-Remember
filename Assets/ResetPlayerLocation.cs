f611f7c2e54ddf054526264b63f2751a0d6426b3
nydahle8784 <50083364+nydahle8784@users.noreply.github.com>
2020-04-06 18:54:32 -0500

    FullProofOfConceptPuzzleWorking
    it works

---
+++ Assets/ResetPlayerLocation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPlayerLocation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject refToResetLocation;
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player" && other is BoxCollider2D)// if colliding with the player, the button can be pushed, and the collider is a BoxCollider2D
+        {
+            other.gameObject.GetComponent<HealthDoctor>().takeDamage(10f); 
+            other.gameObject.transform.position = refToResetLocation.transform.position;
+        }
+    }
+}

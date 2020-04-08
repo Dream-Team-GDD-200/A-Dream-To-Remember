04b5d4462cae8fbe198e172117b2fbca8e86e5fc
Zachary-Boehm <60978390+Zachary-Boehm@users.noreply.github.com>
2020-04-08 13:43:07 -0500

    New Spawner Logic

--- Assets/Scripts/Enemy/Spawner.cs
+++ Assets/Scripts/Enemy/Spawner.cs
@@ -4,31 +4,46 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public int amountOfEnemies; // holds the amount of enemies that you want to spawn
+    [SerializeField]public int amountOfEnemies; // holds the amount of enemies that you want to spawn
+    [Header("Enemy Prefabs")]
     public GameObject[] Enemy; // holds enemy types
+    [Header("Enemies Spawned")]
     public List<GameObject> SpawnedEnemies;//Holds all the enemies that where spawned
+    [Header("Spawn Locations")]
+    public GameObject[] SpawnLocations; // spawnLocations for the enemy
+    [Header("Enemy Type (1 = first only | 2+ = first through 2+)")]
     public int enemyTypes;// 1 --> all enemies through melee , 2 --> all enemies melee to ranged
+    [SerializeField] public int MaxEnemies;
+    private bool[] usedPositions;
     private int rand; // random number
+    [Header("Bools for spawner Activity")]
     public bool allEnemiesDead = false; // are all enemies dead?
     public bool active = false; // did the spawner activate?
     //spawnEnemies() method will spawn the enemies with wanted results
+    private void Start() {
+        MaxEnemies = SpawnLocations.Length;
+        usedPositions = new bool[SpawnLocations.Length];
+        //Sets maximum amount of enemies
+        if (amountOfEnemies > MaxEnemies)
+        {
+            amountOfEnemies = MaxEnemies;
+        }
+    }
     public void spawnEnemies()
     {
-        //Sets maximum amount of enemies
-        if (amountOfEnemies > 4)
-        {
-            amountOfEnemies = 4;
-        }
         if(amountOfEnemies > 0)
         {
             for (int i = 0; i < amountOfEnemies; i++)
             {
-                if (i == 0)//right of spawner
-                {
-                    //randomly spawn enemy type
+                //randomly spawn enemy type
                     rand = Random.Range(0, enemyTypes);
                     // set the position to the right of spawner
-                    Vector2 position = new Vector2(transform.position.x + 1, transform.position.y);
+                    int temp;
+                    do{
+                        temp = Random.Range(0, MaxEnemies);
+                    }while(usedPositions[temp] == true);
+                    Vector2 position = SpawnLocations[temp].transform.position; //TODO: change this line
+                    usedPositions[temp] = true;
                     // spawn enemy
                     GameObject spEnemy = Instantiate(Enemy[rand], position, Quaternion.identity);
                     // add the object to local and master lists
@@ -36,48 +51,6 @@ public class Spawner : MonoBehaviour
                     GameObject.FindGameObjectWithTag("Player").GetComponent<AllThings>().push(spEnemy);
                     // put the new instantiated object inside the heirarchy correctly
                     spEnemy.transform.parent = GameObject.FindGameObjectWithTag("MasterEnemies").transform;
-                }
-                else if (i == 1)//left of spawner
-                {
-                    //randomly spawn enemy type
-                    rand = Random.Range(0, enemyTypes);
-                    // set the position to the left of spawner
-                    Vector2 position = new Vector2(transform.position.x - 1, transform.position.y);
-                    GameObject spEnemy = Instantiate(Enemy[rand], position, Quaternion.identity);
-                    // add the object to local and master lists
-                    SpawnedEnemies.Add(spEnemy);
-                    GameObject.FindGameObjectWithTag("Player").GetComponent<AllThings>().push(spEnemy);
-                    // put the new instantiated object inside the heirarchy correctly
-                    spEnemy.transform.parent = GameObject.FindGameObjectWithTag("MasterEnemies").transform;
-                }
-                else if (i == 2)//above spawner
-                {
-                    //randomly spawn enemy type
-                    rand = Random.Range(0, enemyTypes);
-                    // set the position to the top of spawner
-                    Vector2 position = new Vector2(transform.position.x, transform.position.y + 1);
-                    // spawn enemy
-                    GameObject spEnemy = Instantiate(Enemy[rand], position, Quaternion.identity);
-                    // add the object to local and master lists
-                    SpawnedEnemies.Add(spEnemy);
-                    GameObject.FindGameObjectWithTag("Player").GetComponent<AllThings>().push(spEnemy);
-                    // put the new instantiated object inside the heirarchy correctly
-                    spEnemy.transform.parent = GameObject.FindGameObjectWithTag("MasterEnemies").transform;
-                }
-                else if (i == 3)//below spawner
-                {
-                    //randomly spawn enemy type
-                    rand = Random.Range(0, enemyTypes);
-                    // set the position to the bottom of spawner
-                    Vector2 position = new Vector2(transform.position.x, transform.position.y - 1);
-                    // spawn enemy
-                    GameObject spEnemy = Instantiate(Enemy[rand], position, Quaternion.identity);
-                    // add the object to local and master lists
-                    SpawnedEnemies.Add(spEnemy);
-                    GameObject.FindGameObjectWithTag("Player").GetComponent<AllThings>().push(spEnemy);
-                    // put the new instantiated object inside the heirarchy correctly
-                    spEnemy.transform.parent = GameObject.FindGameObjectWithTag("MasterEnemies").transform;
-                }
             }
             active = true;
         }

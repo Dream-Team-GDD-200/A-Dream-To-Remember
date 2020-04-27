2eeaf49773ab830cb63e90c58a8ae025ef5c4f5c
Zachary-Boehm <60978390+Zachary-Boehm@users.noreply.github.com>
2020-04-27 16:52:26 -0500

    Spawner tweeks
    Changed it so you can change the ratio at which a room spawns enemies. You can adjust the percent chance of each enemy to spawn

--- Assets/Scripts/Enemy/Spawner.cs
+++ Assets/Scripts/Enemy/Spawner.cs
@@ -12,15 +12,17 @@ public class Spawner : MonoBehaviour
     [Header("Spawn Locations")]
     public GameObject[] SpawnLocations; // spawnLocations for the enemy
     [Header("Enemy Type (1 = first only | 2+ = first two enemy types)")]
-    public int enemyTypes;// 1 --> all enemies through melee , 2 --> all enemies melee to ranged
     [SerializeField] public int MaxEnemies;
+    [Range(0, 100)] public int EnemySpawnRatio; // left side of slider is melee enemy and right side of slider is projectile enemy
     private bool[] usedPositions;
     private int rand; // random number
     [Header("Bools for spawner Activity")]
     public bool allEnemiesDead = false; // are all enemies dead?
     public bool active = false; // did the spawner activate?
+
     //spawnEnemies() method will spawn the enemies with wanted results
-    private void Start() {
+    private void Start()
+    {
         MaxEnemies = SpawnLocations.Length;
         usedPositions = new bool[SpawnLocations.Length];
         //Sets maximum amount of enemies
@@ -28,47 +30,60 @@ public class Spawner : MonoBehaviour
         {
             amountOfEnemies = MaxEnemies;
         }
-        for(int i = 0; i < usedPositions.Length; i++){
+        for (int i = 0; i < usedPositions.Length; i++)
+        {
             usedPositions[i] = false;
         }
     }
     public void spawnEnemies()
     {
-        if(amountOfEnemies > 0)
+        if (amountOfEnemies > 0)
         {
             Debug.Log(amountOfEnemies);
             for (int i = 0; i < amountOfEnemies; i++)
             {
                 //randomly spawn enemy type
-                    rand = Random.Range(0, enemyTypes);
-                    // set the position to the right of spawner
-                    int temp;
-                    do{
-                        temp = Random.Range(0, SpawnLocations.Length);
-                    }while(usedPositions[temp] == true);
-                    Debug.Log("Temp " + temp);
-                    Vector2 position = SpawnLocations[temp].transform.position;
-                    Debug.Log("Position: " + position);
-                    usedPositions[temp] = true;
-                    // spawn enemy
-                    GameObject spEnemy = Instantiate(Enemy[rand], position, Quaternion.identity);
-                    // add the object to local and master lists
-                    SpawnedEnemies.Add(spEnemy);
-                    GameObject.FindGameObjectWithTag("Player").GetComponent<AllThings>().push(spEnemy);
-                    // put the new instantiated object inside the heirarchy correctly
-                    spEnemy.transform.parent = GameObject.FindGameObjectWithTag("MasterEnemies").transform;
+                rand = Random.Range(0, 100);
+                // set a random position from list of positions
+                int temp;
+                do
+                {
+                    temp = Random.Range(0, SpawnLocations.Length);
+                } while (usedPositions[temp] == true);
+                Debug.Log("Temp " + temp);
+                Vector2 position = SpawnLocations[temp].transform.position;
+                Debug.Log("Position: " + position);
+                usedPositions[temp] = true;
+
+                // spawn enemy
+                GameObject spEnemy = null;
+                if (rand > EnemySpawnRatio)
+                {
+                    spEnemy = Instantiate(Enemy[1], position, Quaternion.identity);
+                }
+                else if (rand < EnemySpawnRatio)
+                {
+                    spEnemy = Instantiate(Enemy[0], position, Quaternion.identity);
+                }
+
+                // add the object to local and master lists
+                SpawnedEnemies.Add(spEnemy);
+                GameObject.FindGameObjectWithTag("Player").GetComponent<AllThings>().push(spEnemy);
+                // put the new instantiated object inside the heirarchy correctly
+                spEnemy.transform.parent = GameObject.FindGameObjectWithTag("MasterEnemies").transform;
             }
             active = true;
-            for(int i = 0; i < MaxEnemies; i++){
+            for (int i = 0; i < MaxEnemies; i++)
+            {
                 SpawnLocations[i].SetActive(false);
             }
         }
     }
     private void Update()
     {
-        for(int i = 0; i < SpawnedEnemies.Count; i++)
+        for (int i = 0; i < SpawnedEnemies.Count; i++)
         {
-            if(SpawnedEnemies[i] == null)
+            if (SpawnedEnemies[i] == null)
             {
                 SpawnedEnemies.RemoveAt(i);
             }

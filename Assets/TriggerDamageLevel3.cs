e9287d5fd4f97482401f3a236b328d510b62f89a
nydahle8784 <50083364+nydahle8784@users.noreply.github.com>
2020-04-27 23:17:07 -0500

    BossTEMPpuzzles added
    These will be changed later

---
+++ Assets/TriggerDamageLevel3.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDamageLevel3 : MonoBehaviour
+{
+    public FinalBossHandler refToHandler;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        refToHandler.SwitchHit();
+    }
+}
